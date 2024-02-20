fddc872082b48326befbddaa638bf6d6e4480d66
Finlay Metcalfe <finlaymetcalfe@hotmail.com>
2024-02-21 00:49:26 +1300

    Implemented basic permission checker

--- app/src/main/java/com/ofthemasses/self_management_app/MainActivity.kt
+++ app/src/main/java/com/ofthemasses/self_management_app/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.ofthemasses.self_management_app
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,11 +18,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.ofthemasses.self_management_app.diary.DiarySerializer
 import com.ofthemasses.self_management_app.ui.theme.SelfmanagementappTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +38,7 @@ class MainActivity : ComponentActivity() {
             SelfmanagementappTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Middle();
+                    Middle(this);
                 }
             }
         }
@@ -37,7 +46,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Middle() {
+fun Middle(activity: Activity? = null) {
+    var cardText by remember {
+        mutableStateOf("Hello World!")
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -45,11 +58,15 @@ fun Middle() {
     ) {
         ElevatedCard(
             elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
-            modifier = Modifier.size(width = 240.dp, height = 100.dp)
+            modifier = Modifier
+                .size(width = 240.dp, height = 100.dp)
+                .clickable {
+                    cardText = if(DiarySerializer.checkPermission(activity)) "True" else "False";
+                }
         )
         {
             Text(
-                text = "Hello World!",
+                text = cardText,
                 modifier = Modifier.padding(16.dp),
                 textAlign = TextAlign.Center
             )
--- app/src/main/java/com/ofthemasses/self_management_app/diary/DiarySerializer.kt
+++ app/src/main/java/com/ofthemasses/self_management_app/diary/DiarySerializer.kt
@@ -1,7 +1,52 @@
 package com.ofthemasses.self_management_app.diary
 
-class DiarySerializer {
+import android.app.Activity
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+
+class DiarySerializer : Activity(){
+
     companion object {
+
+        private val STORAGE_PERM_CODE = 100;
+
+        private var instance: DiarySerializer? = null;
+
+        private lateinit var DiaryEntries: HashMap<String, DiaryEntry>;
+
+        private fun applicationContext() : Context {
+            return instance!!.applicationContext;
+        }
+
+        //fun DeserializeToday(): DiaryEntry{
+        //}
+
+        fun checkPermission(activity: Activity?) : Boolean {
+            if (activity == null){
+                return false;
+            }
+
+
+            var result = ContextCompat.checkSelfPermission(activity.applicationContext,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                activity.requestPermissions(
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    STORAGE_PERM_CODE
+                )
+
+                return ContextCompat.checkSelfPermission(activity.applicationContext,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+            }
+
+            return true;
+        }
 
     }
 }
